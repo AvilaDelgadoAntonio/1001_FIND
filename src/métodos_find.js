14034d72315604639e061eaa91fa98e52cb0057f
githubaa2020 <antavidel@gmail.com>
2020-10-11 22:19:38 +0200

    first commit

--- src/métodos_find.js
+++ src/métodos_find.js
@@ -1,4 +1,4 @@
 db.películas.find()
 db.películas.find ({"Director": "James Cameron"})
 db.películas.find ({"Director": {$eq: "John Ford"}})
-db.películas.find ({"premios": "4"})
+db.películas.find ({"premios": 4})
