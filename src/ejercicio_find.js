ca8db034ee8d23f8ca420a93e5d1c6d2999b162e
githubaa2020 <antavidel@gmail.com>
2020-10-11 20:39:44 +0200

    first commit

---
+++ src/ejercicio_find.js
@@ -0,0 +1,6 @@
+db.películas.insertMany( [
+    { Título: "Coge el dinero y corre", Director: "Woody Allen", premios: 2 },
+    { Título: "Centauros del Desierto", Director: "John Ford", premios: 4 },
+    { Título: "Terminator", Director: "James Cameron", premios: 4  },
+    { Título: "Titanic", Director: "James Cameron", premios: 7  }
+ ] );
